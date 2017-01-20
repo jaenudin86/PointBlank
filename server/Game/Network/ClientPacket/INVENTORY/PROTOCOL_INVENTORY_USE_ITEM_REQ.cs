d8d59eb06876bc3f7d385eb7f9a2102e61397bc9
Gabriel Trucollo <gabrieltrucollo@gmail.com>
2017-01-20 13:32:49 -0200

    Update PROTOCOL_INVENTORY_USE_ITEM_REQ,cs
    You need to treat each item individually

--- server/Game/Network/ClientPacket/INVENTORY/PROTOCOL_INVENTORY_USE_ITEM_REQ.cs
+++ server/Game/Network/ClientPacket/INVENTORY/PROTOCOL_INVENTORY_USE_ITEM_REQ.cs
@@ -28,16 +28,29 @@ namespace Game.Network.ClientPacket
 
         public override void RunImpl()
         {
+            
             //меджик формула :  itemid - 30 + 1000000000
             Item item = getClient().getPlayer().getItemById(ID);
+            Player player = getClient().getPlayer();            
             int oldID = item.Id;
             ItemsTable.DelItem(oldID);
             int NewID = oldID - 30 + 1000000000;
             int CouponTime = Convert.ToInt32(DateTime.Now.ToString("yyMMddHHmm") + "0000070000");//актуальное время + 7 дней(для теста)
             ItemsTable.AddItem(getClient().getPlayer().PlayerID, NewID, 10, 3, CouponTime);
 
-            getClient().SendPacket((SendPacket)new PROTOCOL_INVENTORY_DELET_ITEM_ACK(ID));
+            switch (NewID) // Need help for get ItemID
+            {
+                case 100040001:
+                    player.setEffect2(player.getEffect2() + 32); // Mega HP 5% 
+                    PlayersTable.UpdateEffect2(player.AccountID, player.getEffect2() + 32);
+                break;
+            } 
+            if ( NewID != 100040001)
+            {
+                getClient().SendPacket((SendPacket)new PROTOCOL_INVENTORY_DELET_ITEM_ACK(ID)); // Verify for me?
+            }            
             getClient().SendPacket((SendPacket)new PROTOCOL_INVENTORY_ADD_ITEM_ACK(NewID));
+        }
         }
     }
 }
